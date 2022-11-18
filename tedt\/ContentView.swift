3eab01ebc063b1c7be41456922e029d5b62fd902
Damian <damheros@gmail.com>
2022-11-18 08:44:15 +0100

    creating new repo for projectda

--- tedt\/ContentView.swift
+++ tedt\/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hel!")
+            Text("H!")
         }
         .padding()
     }
