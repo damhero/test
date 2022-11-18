4cd58cc826d6505d80f6c0aef92730ba8a0205d5
Damian <damheros@gmail.com>
2022-11-18 08:20:51 +0100

    Initial Commit

---
+++ tedt\/tedt_App.swift
@@ -0,0 +1,17 @@
+//
+//  tedt_App.swift
+//  tedt\
+//
+//  Created by Damian Brudkowski on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct tedt_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
